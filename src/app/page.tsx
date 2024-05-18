8fd36da4812daab5f24c24b56011ca601626959f
soyab-mostofa <soyabmostofa@gmail.com>
2024-05-18 13:03:02 +0600

    database connected to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,15 @@ import Image from "next/image";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 // All requests made with the client will be authenticated
-const mockUrls = [
-  "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
-  "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
-  "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
-  "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
-  "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
-  "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
-];
 
-const mockData = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main className="">
       <div className="flex flex-wrap justify-center gap-8">
-        {[...mockData, ...mockData].map((image) => (
+        {[...images, ...images].map((image) => (
           <div key={image.id} className="w-48">
             <Image
               src={image.url}
