3fff197fa657355d63b5ce600d1db63e5d991d65
soyab-mostofa <soyabmostofa@gmail.com>
2024-05-18 11:33:22 +0600

    database configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { db } from "~/server/db";
+export const dynamic = "force-dynamic";
 // All requests made with the client will be authenticated
 const mockUrls = [
   "https://utfs.io/f/bd9d9915-badb-43ed-94e3-6e1dadf44477-vlcusr.jpg",
@@ -13,12 +16,20 @@ const mockData = mockUrls.map((url, index) => ({
   url,
 }));
 export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap justify-center gap-8">
         {[...mockData, ...mockData].map((image) => (
           <div key={image.id} className="w-48">
-            <img src={image.url} alt="image" className="" />
+            <Image
+              src={image.url}
+              width={192}
+              height={"100"}
+              alt="image"
+              className=""
+            />
           </div>
         ))}
       </div>
